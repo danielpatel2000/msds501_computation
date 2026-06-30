26c8063f221c9a2e90717537c5749cb0eef8165b
dudedata <mikecerna99@gmail.com>
2026-06-30 11:19:36 -0500

    Added Try/Except for ValueError

--- partner_b.py
+++ partner_b.py
@@ -33,9 +33,13 @@ scores = ['88', '95', 'absent', '72', 'n/a', '84']
 valid = []
 
 for i, s in enumerate(scores):
-    score = int(s)            # <-- this line crashes on bad strings
-    print(f'Score [{i}]: {score}')
-    valid.append(score)
+    try:
+        score = int(s)            # <-- this line crashes on bad strings
+        print(f'index [{i}]: {score}')
+        valid.append(score)
+    except ValueError:
+        print(f'Skipped bad score:[{i}]: {s}')
+
 
 print(f'Total of valid scores: {sum(valid)}')
 print(f'Count of valid scores: {len(valid)}')
